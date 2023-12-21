45f9b53f6cf9fc49511cf9ae4cc5b574f9b47b73
111miguel111 <miguelmora111pro@gmail.com>
2023-12-21 20:21:11 +0100

    texteo innecesario y poco productivo
    me he quedado en el tutorial 10 minuto 6 de ranged attacks

--- Unity/TFG_Test1/Assets/Scripts/tutorial/playerController.cs
+++ Unity/TFG_Test1/Assets/Scripts/tutorial/playerController.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public float runSpeed;
-    public float shootSpeed;
+    //Importante las variables de unity empiezan con mayuscula las que creamos nosotros en miniscula
+    public float runSpeed;//Velocidad de correr
+    public float shootSpeed;//Velocidad al disparar
 
-    Rigidbody myRB;
-    Animator myAnim;
+    Rigidbody myRB;//El muñeco
+    Animator myAnim;//La informacion del muñeco
 
-    bool facingRight;
+    bool facingRight;//Para saber si esta mirando a la derecha
 
     //for jumping
-    bool grounded = false;
-    Collider[] groundCollisions;
-    float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
-    public Transform groundCheck;
-    public float jumpHeight;
+    bool grounded = false;//Si esta tocando suelo
+    Collider[] groundCollisions;//Lo que va a detectar si esta en el suelo
+    float groundCheckRadius = 0.2f;//La pelota que detecta el suelo
+    public LayerMask groundLayer;//Lo que vamos a entender como suelo
+    public Transform groundCheck;//Lo que unity leera para detectar el suelo
+    public float jumpHeight;//La altura del salto
 
     // Start is called before the first frame update
     void Start()
     {
-        myRB = GetComponent<Rigidbody>();
-        myAnim = GetComponent<Animator>();
+        myRB = GetComponent<Rigidbody>();//Donde aplicamos las fuerzas
+        myAnim = GetComponent<Animator>();//Donde informamos de los cambios en el estado del muñeco para unity
         facingRight = true;
     }
 
@@ -35,14 +36,16 @@ public class playerController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        //jumping code
         if(grounded && Input.GetAxis("Jump") > 0)
         {
             grounded = false;
             myAnim.SetBool("grounded", grounded);
             myRB.AddForce(new Vector3(0, jumpHeight, 0));
         }
+        
 
-        //jump code
+        //landing code
         groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
         if(groundCollisions.Length > 0 )
         {
@@ -54,24 +57,24 @@ public class playerController : MonoBehaviour
         }
         myAnim.SetBool("grounded", grounded);
 
-
-
+        //Para moverse
         float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("speed",Mathf.Abs(move));
         
-
+        //Para moverse disparando
         float shooting = Input.GetAxisRaw("Fire1");
         myAnim.SetFloat("shooting", shooting);
-        if(shooting > 0 && grounded)
+        //Para mover al personaje con las siguientes cndiciones
+        if(shooting > 0 && grounded)//Si esta disparando y esta en el suelo se mueve a la velocidad de disparo
         {
             myRB.velocity = new Vector3(move * shootSpeed, myRB.velocity.y, 0);
         }
-        else
+        else//Si no esta disparando se movera a la velocidad de correr independientemente de si esta en el suelo o en el aire
         {
             myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
         }
         
-
+        //Para girar el personaje
         if (move > 0 && !facingRight)
         {
             Flip();
