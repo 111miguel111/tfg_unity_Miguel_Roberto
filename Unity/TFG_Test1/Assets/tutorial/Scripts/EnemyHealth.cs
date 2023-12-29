0b39a1d0c43c37f318b7e076439ce8795c24de22
111miguel111 <miguelmora111pro@gmail.com>
2023-12-30 00:27:38 +0100

    Sonidos everywhere

---
+++ Unity/TFG_Test1/Assets/tutorial/Scripts/EnemyHealth.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour{
+
+    public float enemyMaxHealth;
+    public float currentHealth;
+    public float damageModifier;
+    public GameObject damageParticles;//Sangre
+    public GameObject deathParticles;//Muerte
+    public GameObject drop;//Lo que dropean
+    public bool drops;//Si dropean algo
+    public AudioClip[] deathSounds;
+    public AudioClip[] damageSounds;
+    AudioSource enemyAS;
+
+    // Start is called before the first frame update
+    void Start(){
+        currentHealth = enemyMaxHealth;
+        enemyAS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void addDamage(float damage){
+        damage = damage * damageModifier;
+        if(damage > 0f){
+            currentHealth -= damage;
+            playSound(damageSounds);
+            damageFX(damageParticles, transform.position, new Vector3(0, 0, 0));
+            if (currentHealth <= 0f){
+                makeDead();
+            }
+            
+        }
+    }
+
+    public void damageFX(GameObject particle, Vector3 point, Vector3 rotation)
+    {
+        Instantiate(particle, point, Quaternion.Euler(rotation));
+    }
+    public void playSound(AudioClip[] sounds)
+    {
+        AudioClip tempClip = sounds[Random.Range(0, sounds.Length)];
+        enemyAS.clip = tempClip;
+        enemyAS.Play();
+    }
+    void makeDead(){
+        //turn off movement
+        //create ragdoll
+        playSound(deathSounds);
+        damageFX(deathParticles, transform.position, new Vector3(0, 0, 0));
+        if (drops){
+            Instantiate(drop, transform.position, drop.transform.rotation);
+        }
+        Destroy(gameObject.transform.root.gameObject);
+        
+    }
+}
