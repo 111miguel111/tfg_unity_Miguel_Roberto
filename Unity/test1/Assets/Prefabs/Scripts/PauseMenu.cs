d5b57c7f4322b375728a33bc3bf51d0b6576f44d
RCast23 <RobertoCastillaSenero@gmail.com>
2024-03-29 20:02:55 +0100

    Los botones no funcionan

---
+++ Unity/test1/Assets/Prefabs/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenu;
+    public GameObject pauseScreen;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            isPaused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
