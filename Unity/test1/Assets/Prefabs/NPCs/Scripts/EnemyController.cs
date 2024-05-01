98ec850e6cf15a475a27c7e62d4d8b55e4ba06a8
Miguel Gonzalez Mora <132752164+111miguel111@users.noreply.github.com>
2024-05-01 11:41:50 +0200

    a

---
+++ Unity/test1/Assets/Prefabs/NPCs/Scripts/EnemyController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent nmAgent;
+    public GameObject player;
+    public bool enemyMove;
+    public bool findPlayer;
+    [Header("opciones")]
+    public bool lockTarget=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nmAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(findPlayer && lockTarget) { 
+        nmAgent.destination = player.transform.position;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (lockTarget)
+            {
+                findPlayer = true;
+                enemyMove = true;
+            }
+            else
+            {
+                nmAgent.destination = player.transform.position;
+                nmAgent.isStopped = false;
+                enemyMove = true;
+            }
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !lockTarget)
+        {
+            nmAgent.isStopped = true;
+            enemyMove = false;
+        }
+    }
+
+}
