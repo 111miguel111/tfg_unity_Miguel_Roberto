87e03ed0b8d21d2d59ac39b05a99ddcedbc9fb08
Miguel Gonzalez Mora <132752164+111miguel111@users.noreply.github.com>
2024-05-14 23:38:17 +0200

    Preparado para prefabs del mapa y sonidos enemigos

--- Unity/test1/Assets/Prefabs/NPCs/Scripts/EnemyController.cs
+++ Unity/test1/Assets/Prefabs/NPCs/Scripts/EnemyController.cs
@@ -9,20 +9,42 @@ public class EnemyController : MonoBehaviour
     public GameObject player;
     public bool enemyMove;
     public bool findPlayer;
+    public AudioClip[] AtackSounds;
+    public AudioClip[] IdleSounds;
+    public float IdleSoundTime;
+    float nextIdleSound;
+    AudioSource enemyAS;
+    Animator animator;
     [Header("opciones")]
     public bool lockTarget=false;
     // Start is called before the first frame update
     void Start()
     {
         nmAgent = GetComponentInParent<NavMeshAgent>();
+        enemyAS = GetComponentInParent<AudioSource>();
+        animator = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(findPlayer && lockTarget) { 
-        nmAgent.destination = player.transform.position;
+            nmAgent.destination = player.transform.position;
         }
+
+        if ( nextIdleSound < Time.time && findPlayer)
+        {
+            AudioClip tempClip = IdleSounds[Random.Range(0, IdleSounds.Length)];
+            enemyAS.clip = tempClip;
+            enemyAS.Play();
+            nextIdleSound = IdleSoundTime + Time.time;
+        }
+        if (animator.GetCurrentAnimatorStateInfo(1).IsTag("Attack") && !enemyAS.isPlaying)
+        {
+            AudioClip tempClip = AtackSounds[Random.Range(0, AtackSounds.Length)];
+            enemyAS.PlayOneShot(tempClip);
+        }
+
     }
     void OnTriggerStay(Collider other)
     {
@@ -33,6 +55,7 @@ public class EnemyController : MonoBehaviour
             {
                 findPlayer = true;
                 enemyMove = true;
+
             }
             else
             {
